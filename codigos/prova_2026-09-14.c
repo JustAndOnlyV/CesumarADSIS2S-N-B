57aed0f2e2c369578ad97f2e999502aba9d0c80b
JustAndOnlyV <313833525+JustAndOnlyV@users.noreply.github.com>
2026-09-14 19:43:41 -0300

    Create prova_2026-09-14.c

---
+++ codigos/prova_2026-09-14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void logarParte(int part, char nome[]){
+    printf("\x1b[36mParte %d: \x1b[32m\"%s\"\x1b[0m",part,nome);
+}
+
+int main(){
+    // Parte 1: numeros divisiveis por 2 ou 3 em {0..20}
+    
+
+    return 0;
+}
